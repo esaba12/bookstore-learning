b32aee3ed4d2d9bd556485bb45482428b9060fed
Ethan Saba <ethansaba12@gmail.com>
2025-06-23 22:09:38 -0700

    Registration and Login Routes Done

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,14 +1,19 @@
 import express from "express";
 import User from "../models/User.js";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
+
+const generateToken = (userId) => {
+  return jwt.sign({userId}, process.env.JWT_SECRET, { expiresIn: "15d"});
+}
 
 router.post("/register", async (req, res) => {
   try {
   const{ email, username, password } = req.body;
 
   // if any field is empty, error
-  if(!username || !email || ~password) {
+  if(!username || !email || !password) {
     return res.status(400).json({ message: "All fields are required"}); 
   }
   // if password is too short, error
@@ -31,11 +36,71 @@ router.post("/register", async (req, res) => {
     return res.status(400).json({message: "Username already exists"});
   }
 
-  } catch (error) {}
+  //pull randon avitar
+  const profileImage = `https://api.dicebear.com/7.x/avataaars/svg?seed=${username}`;
+
+
+
+  const user = new User({
+    email,
+    username,
+    password,
+    profileImage,
+  })
+
+  await user.save();
+
+  const token = generateToken(user._id);
+
+  res.status(201).json({
+    token,
+    user:{
+      _id: user._id,
+      username: user.username,
+      email: user.email,
+      profileImage: user.profileImage,
+    },
+  });
+  } catch (error) {
+    console.log("Error in register route", error);
+    res.status(500).json({ message: "Internal server error" })
+  }
 });
 
 router.post("/login", async (req, res) => {
-  res.send("login");
+  try {
+    const { email, password } = req.body;
+
+    if(!email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    // check if user exists
+    const user = await User.findOne({ email });
+    if(!user) return res.status(400).json({ message: "Invalid credentials" });
+
+    // check if password is correct
+    const isPasswordCorrect = await user.comparePassword(password);
+    if(!isPasswordCorrect) {
+      return res.status(400).json({ message: "Invalid credentials" })
+    }
+
+    // generate token
+    const token = generateToken(user._id);
+
+    res.status(200).json({
+      token,
+      user: {
+        id: user._id,
+        username: user.username,
+        email: user.email,
+        profileImage: user.profileImage,
+      },
+    });
+  } catch (error) {
+    console.log("Error in login route", error);
+    res.status(500).json({ message: "Internal server error"});
+  }
 });
 
 export default router; 
