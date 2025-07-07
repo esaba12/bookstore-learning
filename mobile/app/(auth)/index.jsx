88dcc33221b459b76cacf595ea8c10f341d7b00a
Ethan Saba <ethansaba12@gmail.com>
2025-07-06 23:04:20 -0400

    Login UI Created

---
+++ mobile/app/(auth)/index.jsx
@@ -0,0 +1,104 @@
+import { View, Text, Image, TextInput, TouchableOpacity, ActivityIndicator} from 'react-native'
+import styles from "../../assets/styles/login.styles"
+import { useState } from 'react'
+import COLORS from "../../constants/colors";
+import { Ionicons } from "@expo/vector-icons"
+import { Link } from "expo-router"
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const handleLogin = () => {}
+
+  return (
+    <View style={styles.container}>
+      {/*Illustration */}
+      <View style={styles.topIllustration}>
+        <Image
+          source={require("../../assets/images/coral-reading-glasses.png")}
+          style={styles.illustrationImage}
+          resizeMode="contain" 
+        />
+      </View>
+      <View style={styles.card}>
+        <View style={styles.formContainer}>
+          {/* email */}
+          <View style={styles.inputGroup}>
+            <Text style={styles.label}>Email</Text>
+            <View style={styles.inputContainer}>
+              <Ionicons
+                name="mail-outline"
+                size={20}
+                color={COLORS.primary}
+                style={styles.inputIcon}/>
+              <TextInput 
+                style={styles.input}
+                placeholder="Enter your email"
+                placeholderTextColor={COLORS.placeholderText}
+                value={email}
+                onChangeText={setEmail}
+                keyboardType="email-address"
+                autoCapitalize="none"
+              />
+            </View>
+          </View>
+          {/* Password */}
+          <View style={styles.inputGroup}>
+            <Text style={styles.label}>Password</Text>
+            <View style={styles.inputContainer}>
+              {/* Icon to the left */}
+              <Ionicons
+                name="lock-closed-outline"
+                size={20}
+                color={COLORS.primary}
+                style={styles.inputIcon}/>
+              <TextInput 
+                style={styles.input}
+                placeholder="Enter your password"
+                placeholderTextColor={COLORS.placeholderText}
+                value={password}
+                onChangeText={setPassword}
+                secureTextEntry={!showPassword}
+              />
+              <TouchableOpacity
+                onPress={() => setShowPassword(!showPassword)}
+                style={styles.eyeIcon}
+              >
+                <Ionicons
+                  name={showPassword ? "eye-outline" : 'eye-off-outline'}
+                  size={20}
+                  color={COLORS.primary}
+                />
+              </TouchableOpacity>
+            </View>
+          </View>
+
+          {/* Login Button */ }
+          <TouchableOpacity 
+            style={styles.button}
+            onPress={handleLogin}
+            disabled={isLoading}
+          >
+            {isLoading ? (
+              <ActivityIndicator color="#fff" />
+            ) : (
+              <Text style={styles.buttonText}>Login</Text>
+            )}
+          </TouchableOpacity>
+
+          {/* Footer */}
+          <View style={styles.footer}>
+            <Text style={styles.footerText}>Don't have an account?</Text>
+            <Link href="/signup" asChild>
+              <TouchableOpacity>
+                <Text style={styles.link}>Sign Up</Text>
+              </TouchableOpacity>
+            </Link>
+          </View>
+        </View>
+      </View>
+    </View>
+  )
+}
--- mobile/app/index.jsx
+++ mobile/app/index.jsx
@@ -1,11 +1,13 @@
-import { Image, StyleSheet, Text, View } from "react-native";
-import { Image } from "expo-image"
+import { StyleSheet, Text, View } from "react-native";
+import { Link } from "expo-router";
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Hello</Text>
-      <Image />
+
+      <Link href="/(auth)/signup">Signup Page</Link>
+      <Link href="/(auth)">Login Page</Link>
     </View>
   );
 }
