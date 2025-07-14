defd2f94432b29db31b53b16d82f6280b4686422
Ethan Saba <ethansaba12@gmail.com>
2025-07-13 17:49:38 -0700

    Login and logout pages fully work with backend

--- mobile/app/index.jsx
+++ mobile/app/index.jsx
@@ -1,13 +1,24 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { Link } from "expo-router";
 import { useAuthStore } from "../store/authStore"
+import { useEffect } from "react";
 
 export default function Index() {
-  const { user, token } = useAuthStore();
+  const { user, token, checkAuth, logout } = useAuthStore();
 
+  console.log(user, token);
+
+  useEffect(() => {
+    checkAuth()
+  },[])
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Hello</Text>
+      <Text style={styles.title}>Hello {user?.username}</Text>
+      <Text style={styles.title}>Token: {token} </Text>
+
+      <TouchableOpacity onPress={logout}>
+        <Text>Logout</Text>
+      </TouchableOpacity>
 
       <Link href="/(auth)/signup">Signup Page</Link>
       <Link href="/(auth)">Login Page</Link>
